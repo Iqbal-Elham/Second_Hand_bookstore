604d83b2cb395a49579fa57f1fab12ae97291f8a
iqbal-elham <iqbal.elham7@gmail.com>
2022-10-25 12:43:17 +0430

    render partial installed

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_render_partial'
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request'
             ],
         },
     },
