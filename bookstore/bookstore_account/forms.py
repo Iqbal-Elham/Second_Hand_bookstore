a433c08694d1e7ac27f7c71b118ef980fd2e05d9
iqbal-elham <iqbal.elham7@gmail.com>
2022-11-02 17:44:38 +0430

    Added user register form

--- bookstore/bookstore_account/forms.py
+++ bookstore/bookstore_account/forms.py
@@ -1,4 +1,5 @@
 from django import forms 
+from django.contrib.auth.models import User
 
 
 class login_form(forms.Form):
@@ -15,10 +16,58 @@ class login_form(forms.Form):
 class register_form(forms.Form):
 
     user_name = forms.CharField(
-        widget=forms.TextInput(attrs={"class":"box","placeholder":"Enter your username"}),
-        label="Username"
+        widget=forms.TextInput(attrs={"class":"form-control","placeholder":"Enter your username"}),
+        
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={"class":"form-control","placeholder":"Enter your email"}),
+        
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class":"box","placeholder":"Enter your password"}),
-        label="Password"
+        widget=forms.PasswordInput(attrs={"class":"form-control","placeholder":"Enter your password"}),
+        
     )
+    re_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"class":"form-control","placeholder":"Enter your password again"}),
+        
+    )
+    city = forms.CharField(
+        widget=forms.TextInput(attrs={"class":"form-control","placeholder":"Enter your city"}),
+       
+    )
+    address = forms.CharField(
+        widget=forms.TextInput(attrs={"class":"form-control","placeholder":"Enter your address"}),
+       
+    )
+    phone_num = forms.CharField(
+        widget=forms.TextInput(attrs={"class":"form-control","placeholder":"Enter your phone number"}),
+       
+    )
+    whatsapp_num = forms.CharField(
+        widget=forms.TextInput(attrs={"class":"form-control","placeholder":"Enter your whatsapp number"}),
+       
+    )
+
+    
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        email_is_exist = User.objects.filter(email=email).exists()
+        if email_is_exist:
+            raise forms.ValidationError("Email is exist")
+        return email
+
+    def clean_user_name(self):
+        username = self.cleaned_data.get("user_name")
+        username_is_exist = User.objects.filter(username=username).exists()
+        if username_is_exist:
+            raise forms.ValidationError("Username is exist")
+        return username
+    
+    def clean_re_password(self):
+        password = self.cleaned_data.get("password")
+        re_password = self.cleaned_data.get("re_password")
+
+        if password != re_password:
+            raise forms.ValidationError("Passwords does not match")
+        
+        return password
--- bookstore/bookstore_account/views.py
+++ bookstore/bookstore_account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, get_user_model
-from .forms import login_form
+from django.contrib.auth.models import User
+from .forms import login_form, register_form
 
 # Create your views here.
 
@@ -24,6 +25,22 @@ def login_page(request):
     return render(request, 'account/login.html',context)
 
 def register(request):
-    context = {}
+    if request.user.is_authenticated:
+        return redirect("/")
+    registerForm = register_form(request.POST or None)
+    
+    if registerForm.is_valid():
+        username = registerForm.cleaned_data.get("user_name")
+        email = registerForm.cleaned_data.get("email")
+        password = registerForm.cleaned_data.get("password")
+        city = registerForm.cleaned_data.get("city")
+        address = registerForm.cleaned_data.get("address")
+        phone_num = registerForm.cleaned_data.get("phone_num")
+        whatsapp_num = registerForm.cleaned_data.get("whtatsapp_num")
+        User.objects.create_user(username=username,email=email,password=password)
+        return redirect("/")
+    context = {
+        'registerForm' : registerForm,
+    }
     return render(request, 'account/register.html',context)
 
