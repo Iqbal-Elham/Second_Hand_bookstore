4954f2c35cab433c7eb6bbb6def5091984335f2b
iqbal-elham <iqbal.elham7@gmail.com>
2022-11-14 01:54:21 +0430

    Finished the contact us message forms

--- bookstore/bookstore_contact_us/admin.py
+++ bookstore/bookstore_contact_us/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import contactUs
 # Register your models here.
+
+
+
+class contactAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'full_name', 'is_read']
+    list_filter = ['is_read']
+    list_editable = ['is_read']
+    search_fields = ['subject', 'message']
+
+admin.site.register(contactUs, contactAdmin)
--- bookstore/bookstore_contact_us/models.py
+++ bookstore/bookstore_contact_us/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class contactUs(models.Model):
+
+    full_name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=150)
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    is_read = models.BooleanField()
+
+
+    def __str__(self):
+        return self.subject
